da47cca34efe8a2283dd821ef572031792a6be64
Aditya Aryan <erenyeager200026@gmail.com>
2026-05-30 00:27:36 +0530

    Add file type support message for audio upload

--- client/src/components/analysis/UploadBox.jsx
+++ client/src/components/analysis/UploadBox.jsx
@@ -11,6 +11,10 @@ const UploadBox = ({ selectedFile, setSelectedFile, handleFileChange }) => {
         <>
           <h3 className="text-base font-semibold">Upload Audio</h3>
 
+          <p className="text-gray-500 mt-2 text-xs">
+            MP3, WAV supported
+          </p>
+
           <input
             type="file"
             accept=".mp3,.wav"
